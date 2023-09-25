5804ef62231d491e7bd6aafd2c503f9e9bfa32c9
basagoitiam <marika.basagoitia@gmail.com>
2023-09-25 17:03:40 -0400

    changed background photo and added overlay boxes to home page

--- src/client/src/pages/Home.jsx
+++ src/client/src/pages/Home.jsx
@@ -1,19 +1,19 @@
-import backgroundImage from "../images/mountain-bg.svg";
-import pineTree from "../images/pine-tree.svg";
-import pineTree2 from "../images/pine-tree-2.svg";
-
 function Home () {
     return (
         <>
         <div className="background">
-            <img src={backgroundImage} alt="mountain background" id="mountain-bg" />
-        </div>
-        <div className="overlay">
-            <img src={pineTree2} alt="pine tree" />
-            <img src={pineTree2} alt="pine tree" />
-            <img src={pineTree2} alt="pine tree" />
-            <img src={pineTree} alt="pine tree" />
-            <img src={pineTree2} alt="pine tree" />
+            <div className="overlay-container">
+                <div className="overlay-box">
+                    <h1 className="overlay-content title">Ready for your next Colorado Fourteener Adventure?</h1>
+                    <p className="overlay-content text">Let us help you find the perfect fit.</p>
+                </div>
+                <div className="overlay-box">
+                    <p className="overlay-content text">We will match your skill level and preferences and choose which of the 58 fourteeners you should conquer next. Get the most appropriate route for your skill level.</p>
+                </div>
+                <div className="overlay-box">
+                    <p className="overlay-content text">Real-time weather data, trail information, map (icons)</p>
+                </div>
+            </div>
         </div>
         </>
     )
