fbc738b07610aea288f767d5b118a619bd10893e
basagoitiam <marika.basagoitia@gmail.com>
2023-11-20 20:09:29 -0500

    changed svg to render conditionally based on expand state, and added box shadow to peaks list filter

--- src/client/src/components/PeaksListFilter.jsx
+++ src/client/src/components/PeaksListFilter.jsx
@@ -49,15 +49,41 @@ const PeaksListFilter = ({ editMode, setEditMode, peaks, setNewCompletedPeaks, h
   return editMode ? (
     <div className="mt-4">
       <div className="addPeaksBar">
-        <span onClick={toggleExpand}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="mb-1 mx-1" viewBox="0 0 16 16">
-            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-          </svg>
-          Add Peaks
-        </span>
+      <span onClick={toggleExpand}>
+      {isExpanded ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="mb-1 mx-1"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+          />
+          <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="mb-1 mx-1"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+          />
+          <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+        </svg>
+      )}
+      Add Peaks
+    </span>
+
       </div>
-      <div style={{ maxHeight: isExpanded ? '25vh' : '5vh', overflowY: 'scroll' }}>
+      <div style={{ maxHeight: isExpanded ? '25vh' : '5vh', overflowY: 'scroll' }} className="peaks-list-filter mt-2">
         {isExpanded && (
           <div>
             <SearchBar onInputChange={handleInputChange} />
@@ -66,7 +92,7 @@ const PeaksListFilter = ({ editMode, setEditMode, peaks, setNewCompletedPeaks, h
                 <ListGroup.Item
                   key={idx}
                   onClick={() => handleItemClick(peak)}
-                  className={isPeakSelected(peak) ? 'selectedListItem' : ''}
+                  className={`peakListItem ${isPeakSelected(peak) ? 'selectedListItem' : ''}`}
                 >
                   {peak.name}
                 </ListGroup.Item>
