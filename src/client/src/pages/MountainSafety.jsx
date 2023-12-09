d0dfaf26cae3fdd10c6f6cf65c623de11876fa41
basagoitiam <marika.basagoitia@gmail.com>
2023-12-08 21:31:18 -0500

    adjusted styles for mountain safety page

--- src/client/src/pages/MountainSafety.jsx
+++ src/client/src/pages/MountainSafety.jsx
@@ -5,8 +5,11 @@ import Col from 'react-bootstrap/Col';
 function MountainSafety () {
     return (
         <Container className="mountain-safety">
+            <Row className="mb-4">
             <h1>Mountain Safety and Hiking Fourteeners in Colorado</h1>
-            <p>Hiking Colorado's fourteeners (mountains exceeding 14,000 feet in elevation) offers breathtaking views and thrilling adventures. However, these peaks pose unique challenges and risks. Prioritize safety with thorough preparation, knowledge, and adherence to best practices. Bring the following items with you on <i>every</i> hiking trip.</p>
+            <p className="mt-4">Hiking Colorado's fourteeners (mountains exceeding 14,000 feet in elevation) offers breathtaking views and thrilling adventures. However, these peaks pose unique challenges and risks. Prioritize safety with thorough preparation, knowledge, and adherence to best practices. Bring the following items with you on <i>every</i> hiking trip.</p>
+            </Row>
+            <Row className="mb-4">
             <h2><strong>Ten Hiking Essentials:</strong></h2>
             <ol>
                 <li><strong>Navigation Tools:</strong> Maps, compass, GPS device, or a reliable smartphone app.</li>
@@ -20,24 +23,24 @@ function MountainSafety () {
                 <li><strong>Hydration:</strong> Sufficient water and a filtration system or purification tablets.</li>
                 <li><strong>Emergency Shelter:</strong> Lightweight tent, bivy sack, or space blanket.</li>
             </ol>
+            </Row>
+            <Row className="mb-4">
             <h2><strong>Weather Conditions</strong></h2>
             <ul>
                 <li><strong>Summer:</strong> Be prepared for sudden thunderstorms, usually in the afternoon. Start early to avoid afternoon weather hazards.</li>
                 <li><strong>Winter:</strong> Extreme cold, deep snow, and avalanche risk. Specialized gear, training, and knowledge are essential.</li>
                 <li><strong>Shoulder Seasons (Spring/Fall):</strong> Unpredictable weather. Expect rapid changes in temperature, snow, and high winds.</li>
             </ul>
-            <h2><strong>Preparation for Different Hike Lengths and Difficulties:</strong></h2>
-            <ul>
-                <li><strong>Shorter Hikes:</strong> Even short hikes require essentials. Be aware of trail conditions, elevation gain, and weather forecasts.</li>
-                <li><strong>Moderate Hikes:</strong> Plan with greater detail. Acquire specific route information, assess fitness levels, and estimate time accurately.</li>
-                <li><strong>Longer, More Challenging Hikes:</strong> Rigorous preparation is crucial. Understand the route thoroughly, acclimate to altitude, and carry extra supplies.</li>
-            </ul>
+            </Row>
+            <Row className="mb-4">
             <h2><strong>Training:</strong></h2>
             <ul>
                 <li><strong>Physical Fitness:</strong> Improve cardiovascular endurance and leg strength. Gradually increase hiking distances and elevation gain to prepare for the demands of higher peaks.</li>
                 <li><strong>Altitude Acclimatization:</strong> Gradually ascend, take frequent breaks, and hydrate well. Acclimate to higher elevations before attempting strenuous hikes.</li>
                 <li><strong>Skill Building:</strong> Gain experience in navigation, recognizing weather patterns, and emergency procedures. Consider taking wilderness first aid courses.</li>
             </ul>
+            </Row>
+            <Row className="mb-4">
             <h2><strong>Additional Safety Tips:</strong></h2>
             <ul>
                 <li><strong>Know Your Limits:</strong> Respect your fitness, experience, and the mountain's challenges.</li>
@@ -47,12 +50,25 @@ function MountainSafety () {
                 <li><strong>Gear Check:</strong> Ensure equipment reliability and suitability for the terrain and weather.</li>
             </ul>
             <p>Hiking Colorado's fourteeners is an incredible experience but demands careful preparation and respect for nature's challenges. Prioritize safety, stay informed, and enjoy the stunning landscapes responsibly. Always remember that mountain safety is paramount. This guide provides a comprehensive overview, but detailed planning and preparation are crucial for a safe and enjoyable hike.</p>
+            </Row>
+            <Row>
+                <h2><strong>Additional Resources:</strong></h2>
+                <ul>
+                    <li><strong>14ers.com:</strong> This website offers detailed route descriptions, trip reports, and forums discussing Colorado's fourteeners. It provides valuable information on trail conditions, route variations, and user experiences.</li>
+                    <li><strong>National Weather Service (NWS) - Colorado:</strong> The NWS provides detailed weather forecasts and alerts specific to Colorado's mountain regions, crucial for planning hiking trips and staying updated on changing weather conditions.</li>
+                    <li><strong>AllTrails:</strong> AllTrails is a popular platform offering a wide range of hiking trail information, including user reviews, trail maps, difficulty levels, and photos. It covers various hiking trails worldwide, including those leading to Colorado's fourteeners.</li>
+                    <li><strong>Colorado Trail Explorer (COTREX):</strong> COTREX is an interactive trail map resource providing detailed information on trails across Colorado, including those leading to the state's fourteeners. It offers trail conditions, difficulty ratings, and downloadable maps.</li>
+                    <li><strong>Mountain Forecast:</strong> Mountain Forecast specializes in weather forecasts for mountainous regions. It offers detailed weather predictions specifically tailored for higher elevations, including Colorado's fourteeners.</li>
+                </ul>
+            </Row>
+            <Row className="mb-4">
             <h2><strong>Disclaimer:</strong></h2>
             <p>The information provided on this website is intended as a guide to hiking trails and outdoor activities. It does not assume responsibility for the decisions made by hikers or outdoor enthusiasts. While we aim to offer accurate and helpful information, we cannot guarantee the accuracy, completeness, or suitability of the content for individual circumstances.</p>
             <p>Each person accessing this information is responsible for their own safety and well-being. Hiking, especially in mountainous regions such as Colorado's fourteeners, carries inherent risks that can be influenced by changing weather conditions, personal fitness levels, equipment, and other variables beyond our control.</p>
             <p>The recommendations and trail information presented here are intended as suggestions for hikes that users may find enjoyable. It is essential for individuals to assess their own capabilities, experience, and preparedness before embarking on any outdoor activity. Knowing personal limits, understanding the risks involved, and making informed decisions are crucial for a safe and enjoyable outdoor experience.</p>
             <p>By using this website, you acknowledge that the provided information is not a substitute for personal judgment or professional guidance. We strongly encourage users to conduct their own research, gather updated information, and exercise caution when planning and executing outdoor adventures.</p>
             <p>Remember, each individual is accountable for their choices and actions while engaging in outdoor activities.</p>
+            </Row>
         </Container>
     )
 }
