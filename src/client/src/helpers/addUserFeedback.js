04294c4afeb03a67c5dea78bbb2b3fa3bec2c47d
basagoitiam <marika.basagoitia@gmail.com>
2024-01-12 19:55:53 -0500

    addded feedback router and logic to add feedback data to tables in the database

---
+++ src/client/src/helpers/addUserFeedback.js
@@ -0,0 +1,24 @@
+const addUserFeedback = async (userFeedback) => {
+    try {
+        const response = await fetch('http://localhost:5000/submit-feedback', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            },
+        credentials: 'include',
+        body: JSON.stringify({
+            userFeedback: userFeedback
+        })
+        });
+
+        if (response.ok) {
+        console.log("Feedback successfully submitted");
+        } else {
+            console.log("Error submitting feedback")
+        }
+    } catch (error) {
+        console.error('Error submitting feedback:', error);
+    }
+};
+
+export default addUserFeedback;
--- src/client/src/pages/FeedbackForm.jsx
+++ src/client/src/pages/FeedbackForm.jsx
@@ -7,6 +7,7 @@ import FutureUse from '../components/feedbackFormQuestions/FutureUse';
 import OverallExperience from '../components/feedbackFormQuestions/OverallExperience';
 import Improvements from '../components/feedbackFormQuestions/Improvements';
 import MountainSpecificFeedback from '../components/feedbackFormQuestions/MountainSpecificFeedback';
+import addUserFeedback from '../helpers/addUserFeedback';
 
 function FeedbackForm ({ user }) {
 
@@ -19,15 +20,22 @@ function FeedbackForm ({ user }) {
   const [relevance, setRelevance] = useState(0);
   const [futureUse, setFutureUse] = useState(0);
   const [overallExperience, setOverallExperience] = useState(0);
-  const [improvements, setImprovements] = useState('');
-  const [mountainSpecificFeedback, setMountainSpecificFeedback] = useState('');
+  const [improvements, setImprovements] = useState({});
+  const [mountainSpecificFeedback, setMountainSpecificFeedback] = useState("");
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
     // You will want to send a fetch request including the user's credentials and the peak id.
+    // Add in the list of peaks to choose from.
     // Store the text answers in the database for admin access, and send the numeric ratings to the database
     // for further processing which will include adding to the overall average of all users' responses.
-    console.log({
-      // Currently i am getting NaN for overall experience and relevance. Check this.
+    // Show mountain specific feedback, along with numeric feedback average scores, on the browse all peaks page.
+    // Add a link on the results page for each peak to the link that allows users to browse the reviews/ratings
+    // Include number of ratings
+
+    const userFeedback = {
+      userId: user.id,
+      peakId: peak.id,
       effectiveness,
       usability,
       relevance,
@@ -35,12 +43,15 @@ function FeedbackForm ({ user }) {
       overallExperience,
       improvements,
       mountainSpecificFeedback,
-    });
+    };
+
+    addUserFeedback(userFeedback);
+    
   };
 
   return (
     <Container>
-      <Form>
+      <Form onSubmit={(e) => handleSubmit(e)}>
         <h1>Provide Feedback on the 14er Summit Selector Tool</h1>
         <p>Please answer the questions below and provide any suggestions you have to improve this feature.</p>
 
@@ -53,7 +64,7 @@ function FeedbackForm ({ user }) {
         <FutureUse setFutureUse={setFutureUse} />
         <Improvements improvements={improvements} setImprovements={setImprovements} />
         <OverallExperience setOverallExperience={setOverallExperience} />
-        <Button variant="primary" onClick={handleSubmit}>Submit Feedback</Button>
+        <Button variant="primary" type="submit">Submit Feedback</Button>
       </Form>
     </Container>
   );
