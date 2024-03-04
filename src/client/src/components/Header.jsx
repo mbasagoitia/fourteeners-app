fbf63d189fdfa0f5df5b5cbd9240b4dc2ba9f39f
basagoitiam <marika.basagoitia@gmail.com>
2024-03-04 09:02:32 -0500

    styled navbar to have black background color on small screen sizes

--- src/client/src/components/Header.jsx
+++ src/client/src/components/Header.jsx
@@ -1,10 +1,16 @@
+import { useState } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link, useNavigate } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
 
 function Header ({ user, setUser }) {
+
+    const [navOpen, setNavOpen] = useState(false);
+
+    const handleNavToggle = () => {
+      setNavOpen(!navOpen);
+    }
 
     const navigate = useNavigate();
 
@@ -32,41 +38,39 @@ function Header ({ user, setUser }) {
     const userSvgIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"><path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/><path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/></svg>';
 
     return (
-        <Navbar expand="md" className="sticky-top navbar-dark header">
-            <Container fluid className="m-0 w-100 text-center">
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto w-100 d-flex justify-content-evenly flex-wrap">  
-                <Nav.Link as={Link} to="/" dangerouslySetInnerHTML={{ __html: homeSvgIcon }} />
-                    <NavDropdown title="Summit Selector" id="selector-tool-dropdown">
-                      <NavDropdown.Item as={Link} to={"/summit-selector"}>14er Summit Selector</NavDropdown.Item>
-                      <NavDropdown.Item as={Link} to={"/provide-feedback"}>Rate/Review the Tool</NavDropdown.Item>
-                    </NavDropdown>
-                    <NavDropdown title="Mountain Information" id="mountain-info-dropdown">
-                      <NavDropdown.Item as={Link} to={"/mountain-classification-guide"}>Mountain Classification Guide</NavDropdown.Item>
-                      <NavDropdown.Item as={Link} to={"/mountain-ranges"}>Mountain Ranges of Colorado</NavDropdown.Item>
-                      <NavDropdown.Item as={Link} to={"/mountain-safety"}>Mountain Safety</NavDropdown.Item>
-                      <NavDropdown.Item as={Link} to={"/browse-all-peaks"}>Browse All Peaks</NavDropdown.Item>
-                    </NavDropdown>
-                    <NavDropdown title={<span dangerouslySetInnerHTML={{ __html: userSvgIcon }} />} id="user-dropdown">
-                    {user ? (
-                    <>
-                    <NavDropdown.Item as={Link} to="/my-list">My List</NavDropdown.Item>
-                    <NavDropdown.Item as={Link} to="/manage-account">Manage Account</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item as={Link} onClick={handleLogout}>Log Out</NavDropdown.Item>
-                    </>
-                    ) : (
-                    <>
-                    <NavDropdown.Item as={Link} to="/login">Log In</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item as={Link} to="/register">Register</NavDropdown.Item>
-                    </>
-                    )}
-                    </NavDropdown>
-                </Nav>
-                </Navbar.Collapse>
-                </Container>    
+        <Navbar expand="md" className={`sticky-top navbar-dark header ${navOpen ? "navbar-open" : ""}`} style={{ transition: "background-color 0.5s ease" }}>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleNavToggle}/>
+          <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto w-100 d-flex justify-content-evenly flex-wrap">  
+          <Nav.Link as={Link} to="/" dangerouslySetInnerHTML={{ __html: homeSvgIcon }} />
+              <NavDropdown title="Summit Selector" id="selector-tool-dropdown">
+                <NavDropdown.Item as={Link} to={"/summit-selector"}>14er Summit Selector</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to={"/provide-feedback"}>Rate/Review the Tool</NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="Mountain Information" id="mountain-info-dropdown">
+                <NavDropdown.Item as={Link} to={"/mountain-classification-guide"}>Mountain Classification Guide</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to={"/mountain-ranges"}>Mountain Ranges of Colorado</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to={"/mountain-safety"}>Mountain Safety</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to={"/browse-all-peaks"}>Browse All Peaks</NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title={<span dangerouslySetInnerHTML={{ __html: userSvgIcon }} />} id="user-dropdown">
+              {user ? (
+              <>
+              <NavDropdown.Item as={Link} to="/my-list">My List</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/manage-account">Manage Account</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item as={Link} onClick={handleLogout}>Log Out</NavDropdown.Item>
+              </>
+              ) : (
+              <>
+              <NavDropdown.Item as={Link} to="/login">Log In</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item as={Link} to="/register">Register</NavDropdown.Item>
+              </>
+              )}
+              </NavDropdown>
+          </Nav>
+          </Navbar.Collapse>  
         </Navbar>
     )
 }
