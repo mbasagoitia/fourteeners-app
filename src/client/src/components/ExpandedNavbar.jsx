d86dffe7aea392c50c9f291d011ec17ece09c92d
basagoitiam <marika.basagoitia@gmail.com>
2024-03-07 18:41:54 -0500

    added icons and transitions to navbar

--- src/client/src/components/ExpandedNavbar.jsx
+++ src/client/src/components/ExpandedNavbar.jsx
@@ -17,7 +17,7 @@ const ExpandedNavbar = ({ user, setUser, navigate }) => {
 
     const homeSvgIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"><path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5"/></svg>';
     const userSvgIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"><path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/><path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/></svg>';
-
+    const caretSvgIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16"><path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/></svg>';
     return (
         <>
         <Navbar
@@ -26,8 +26,12 @@ const ExpandedNavbar = ({ user, setUser, navigate }) => {
           style={{ transition: 'background-color 0.5s ease' }}
         >
         <Nav.Link className="expNavLinkTitle" as={Link} to="/" dangerouslySetInnerHTML={{ __html: homeSvgIcon }} />
-        <p className="expNavLinkTitle" onMouseEnter={handleLinkHover}>Summit Selector</p>
-        <p className="expNavLinkTitle" onMouseEnter={handleLinkHover}>Mountain Information</p>
+        <p className="expNavLinkTitle" onMouseEnter={handleLinkHover}>Summit Selector<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="mx-2" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
+        </svg></p>
+        <p className="expNavLinkTitle" onMouseEnter={handleLinkHover}>Mountain Information<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="mx-2" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
+        </svg></p>
         <p className="expNavLinkTitle" onMouseEnter={handleLinkHover} dangerouslySetInnerHTML={{ __html: userSvgIcon }} />
         </Navbar>
         <div className={`nav-drawer ${isDrawerOpen ? "nav-drawer-open" : "nav-drawer-closed"} w-100`} onMouseLeave={handleDrawerClose}>
--- src/client/src/components/NavDrawer.jsx
+++ src/client/src/components/NavDrawer.jsx
@@ -5,8 +5,8 @@ import handleLogout from '../helpers/handleLogout';
 
 const NavDrawer = ({ user, setUser, navigate, handleDrawerClose, className }) => {
   
-    const NavLink = ({to, title}) => {
-      return <Nav.Link className="expNavLink" as={Link} to={to} onClick={handleDrawerClose}>{title}</Nav.Link>
+    const NavLink = ({to, title, className}) => {
+      return <Nav.Link className={`${className} expNavLink`} as={Link} to={to} onClick={handleDrawerClose}>{title}</Nav.Link>
     }
 
     const homeSvgIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"><path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5"/></svg>';
@@ -19,13 +19,13 @@ const NavDrawer = ({ user, setUser, navigate, handleDrawerClose, className }) =>
           </div>
           <div>
               <NavLink to="/summit-selector" title="14er Summit Selector" />
-              <NavLink to="/provide-feedback" title="Rate/Review the Tool" />
+              <NavLink to="/provide-feedback" title="Rate/Review the Tool" className="no-border" />
           </div>
           <div>
               <NavLink to="/mountain-classification-guide" title="Mountain Classification Guide" />
               <NavLink to="/mountain-ranges" title="Mountain Ranges of Colorado" />
               <NavLink to="/mountain-safety" title="Mountain Safety" />
-              <NavLink to="/browse-all-peaks" title="Browse All Peaks" />
+              <NavLink to="/browse-all-peaks" title="Browse All Peaks" className="no-border" />
           </div>
           <div>
           {user ? (
@@ -33,7 +33,7 @@ const NavDrawer = ({ user, setUser, navigate, handleDrawerClose, className }) =>
           <NavLink to="/my-list" title="My List" />
           <NavLink to="/manage-account" title="Manage Account" />
           <NavDropdown.Divider />
-          <Nav.Link as={Link} className="expNavLink mt-4" onClick={() => {
+          <Nav.Link as={Link} className="expNavLink mt-4 no-border" onClick={() => {
               handleLogout(setUser, navigate);
               handleDrawerClose();
           }}>Log Out</Nav.Link>
@@ -42,7 +42,7 @@ const NavDrawer = ({ user, setUser, navigate, handleDrawerClose, className }) =>
           <>
           <NavLink to="/login" title="Log In" />
           <NavDropdown.Divider />
-          <NavLink to="/register" title="Register" />
+          <NavLink to="/register" title="Register" className="no-border" />
           </>
           )}
           </div>
