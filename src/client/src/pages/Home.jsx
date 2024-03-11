23826b6456fcf9de6c0e545e8a4523a793b3600a
basagoitiam <marika.basagoitia@gmail.com>
2024-03-11 19:12:39 -0400

    changed mobile font sizes on recommendations page to fit better with layour

--- src/client/src/pages/Home.jsx
+++ src/client/src/pages/Home.jsx
@@ -5,6 +5,12 @@ import { WiRain } from 'react-icons/wi';
 import { FaCheckDouble } from 'react-icons/fa';
 import { GiTrail } from 'react-icons/gi';
 
+// Find all svgs and replace them
+// Finish styling recommendations page
+// Photo and editing layovers need styling
+// Check all mobile sizes and font sizes
+// Proxies
+
 function Home() {
   const [fadeIn, setFadeIn] = useState(false);
   const [showWeather, setShowWeather] = useState(false);
@@ -12,15 +18,12 @@ function Home() {
   const [showTrailInfo, setShowTrailInfo] = useState(false);
 
   useEffect(() => {
-    // Trigger the fade-in animation on page load
     setFadeIn(true);
 
-    // Display each box with a delay
     const weatherTimer = setTimeout(() => setShowWeather(true), 500);
     const recsTimer = setTimeout(() => setShowRecs(true), 1000);
     const trailInfoTimer = setTimeout(() => setShowTrailInfo(true), 1500);
 
-    // Clear timers on component unmount
     return () => {
       clearTimeout(weatherTimer);
       clearTimeout(recsTimer);
@@ -30,42 +33,38 @@ function Home() {
 
   return (
     <div className="content-container">
-        <div className="overlay-container">
-            <div className="fullsize-overlay-box">
-            <div className={`home-container ${fadeIn ? 'fade-in' : ''}`}>
+      <div className="overlay-container">
+        <div className="fullsize-overlay-box">
+          <div className={`home-container ${fadeIn ? 'fade-in' : ''}`}>
             <div className="content">
-                <h1 className="title">Summit Selector</h1>
-                <p className="tagline">Your guide to the Colorado Fourteeners</p>
-                <p className="description">
-                Discover the perfect peak and route for your next adventure. Let us guide you through the tallest and most majestic peaks in the state.
-                </p>
-                <Link className="btn btn-primary" to="/summit-selector" id="get-started-btn">
-                Get Started
-                </Link>
+              <h1 className="title">Summit Selector</h1>
+              <p className="tagline">Your guide to the Colorado Fourteeners</p>
+              <p className="description">Discover the perfect peak and route for your next adventure. Let us guide you through the tallest and most majestic peaks in the state.</p>
+              <Link className="btn btn-primary" to="/summit-selector" id="get-started-btn">Get Started</Link>
             </div>
             <div className="hp-box-container">
-            <div className={`info-box ${showWeather ? 'fade-in' : ''}`}>
-            <div className="weather-info">
-                <p className="overlay-content text">Current Weather Data</p>
-                <WiRain size={40} style={{ color: "#d48106" }} />
-            </div>
-            </div>
-            <div className={`info-box ${showRecs ? 'fade-in' : ''}`}>
-                <div className="personalized-recommendations">
-                    <p className="overlay-content text">Personalized Recommendations</p>
-                    <FaCheckDouble size={25} style={{ color: "#d48106" }} />
+              <div className={`info-box ${showWeather ? 'fade-in' : ''}`}>
+                <div className="weather-info">
+                  <p className="overlay-content text">Current Weather Data</p>
+                  <WiRain size={40} style={{ color: "#d48106" }} />
                 </div>
-            </div>
-            <div className={`info-box ${showTrailInfo ? 'fade-in' : ''}`}>
-            <div className="trail-info">
-                <p className="overlay-content text">Trail Information</p>
-                <GiTrail size={30} style={{ color: "#d48106" }} />
-            </div>
-            </div>
-            </div>
-            </div>
-            </div>
+              </div>
+              <div className={`info-box ${showRecs ? 'fade-in' : ''}`}>
+                <div className="personalized-recommendations">
+                  <p className="overlay-content text">Personalized Recommendations</p>
+                  <FaCheckDouble size={25} style={{ color: "#d48106" }} />
+                </div>
+              </div>
+              <div className={`info-box ${showTrailInfo ? 'fade-in' : ''}`}>
+                <div className="trail-info">
+                  <p className="overlay-content text">Trail Information</p>
+                  <GiTrail size={30} style={{ color: "#d48106" }} />
+                </div>
+              </div>
+          </div>
+          </div>
         </div>
+      </div>
     </div>
   );
 }
